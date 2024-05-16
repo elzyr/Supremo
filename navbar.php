ac9e81cff93bb6b390774eeb01e5c8f7d925df06
KHUJOLT <ilya.khotim@gmail.com>
2024-05-16 20:00:08 +0200

    Update navbar.php
    Dodano zakladke karta ocen do navbara

--- navbar.php
+++ navbar.php
@@ -17,6 +17,7 @@
             <li <?php if(basename($_SERVER['PHP_SELF']) == 'Eventy.php') echo 'class="active"'; ?>><a href="Eventy.php"><i class="fas fa-calendar-alt"></i> Eventy</a></li>
             <li <?php if(basename($_SERVER['PHP_SELF']) == 'Kalendarz.php') echo 'class="active"'; ?>><a href="Kalendarz.php"><i class="fas fa-calendar"></i> Kalendarz</a></li>
             <li <?php if(basename($_SERVER['PHP_SELF']) == 'Plan_tygodnia.php') echo 'class="active"'; ?>><a href="Plan_tygodnia.php"><i class="fas fa-tasks"></i> Plan tygodnia</a></li>
+            <li <?php if(basename($_SERVER['PHP_SELF']) == 'Karta_ocen.php') echo 'class="active"'; ?>><a href="Karta_ocen.php"><i class="fas fa-clipboard-list"></i> Karta ocen</a></li>
         </ul>
     </div>
     <div class="right-nav">
