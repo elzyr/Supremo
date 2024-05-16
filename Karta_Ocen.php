07eeaed5a953ad654d458f01957684d8916f25cc
KHUJOLT <ilya.khotim@gmail.com>
2024-05-16 19:59:28 +0200

    utworzono plik Karta_Ocen.php
    utworzono plik wraz z zaimportowanymi czcionkami oraz troche css

---
+++ Karta_Ocen.php
@@ -0,0 +1,61 @@
+<?php include 'navbar.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Karta Ocen</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: whitesmoke;
+            font-family: 'Inter', sans-serif;
+        }
+
+        .header {
+            background-color: #3180C9;
+            font-weight: 400;
+            font-size: 34px;
+        }
+
+        .left-nav {
+            font-family: 'Poppins', sans-serif;
+            font-weight: 400;
+            line-height: 27px;
+        }
+
+        .right-nav {
+            font-family: 'Poppins', sans-serif;
+            font-weight: 400;
+        }
+
+        .logout-btn .fas {
+            margin-right: 15px;
+        }
+
+        .navbar {
+            background-color: #fff;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        .navbar-brand {
+            color: #000;
+            margin-left: 30px;
+            font-weight: 600;
+            font-size: 26px;
+        }
+    </style>
+</head>
+
+<body>
+
+
+</body>
+
+</html>
