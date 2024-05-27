dceb7864319cbc7d25e49333afd5c91960410bf8
KHUJOLT <ilya.khotim@gmail.com>
2024-05-27 12:03:01 +0200

    edited top-navbar
    dodany css
    dodany "burger"

--- top-navbar.php
+++ top-navbar.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,31 +12,67 @@
         }
 
         .navbar {
-            height: 80px; /* Zmiana wysokości paska nawigacyjnego na 50 pikseli */
-            width: calc(100% - 200px); /* Rozciągamy do końca ekranu po odjęciu 200 pikseli */
-            margin-left: 200px; /* Początek paska nawigacyjnego od 200 pikseli */
-            background-color: #333; /* Kolor tła paska nawigacyjnego */
-            color: #fff; /* Kolor tekstu */
-            padding: 10px 20px; /* Wewnętrzny padding */
-            box-sizing: border-box; /* Uwzględniamy padding w szerokości */
-            display: flex; /* Flexbox dla centrowania elementów */
-            justify-content: space-between; /* Wyrównanie elementów do lewej i prawej strony */
-            align-items: center; /* Wyrównanie elementów w pionie */
+            height: 80px;
+            /* Zmiana wysokości paska nawigacyjnego na 50 pikseli */
+            width: calc(100% - 200px);
+            /* Rozciągamy do końca ekranu po odjęciu 200 pikseli */
+            margin-left: 200px;
+            /* Początek paska nawigacyjnego od 200 pikseli */
+            background-color: #333;
+            /* Kolor tła paska nawigacyjnego */
+            color: #fff;
+            /* Kolor tekstu */
+            padding: 10px 20px;
+            /* Wewnętrzny padding */
+            box-sizing: border-box;
+            /* Uwzględniamy padding w szerokości */
+            display: flex;
+            /* Flexbox dla centrowania elementów */
+            justify-content: flex-start;
+            /* Wyrównanie elementów do lewej i prawej strony */
+            align-items: center;
+            /* Wyrównanie elementów w pionie */
         }
 
         .navbar-brand {
-            margin-left: 10px; /* Przesunięcie napisu w prawo o 5 pikseli */
-            font-weight: bold; /* Pogrubienie tekstu */
-            font-size: 25px; /* Zwiększenie rozmiaru czcionki */
+            margin-left: 10px;
+            /* Przesunięcie napisu w prawo o 5 pikseli */
+            font-weight: bold;
+            /* Pogrubienie tekstu */
+            font-size: 25px;
+            /* Zwiększenie rozmiaru czcionki */
         }
 
-        
+        .burger-icon {
+            font-size: 24px;
+            background: none;
+            border: none;
+            color: white;
+            cursor: pointer;
+            margin-right: 10px;
+        }
+
+        .hidden-burger {
+            display: none;
+        }
+
+        .collapsed-navi {
+            display: none;
+        }
+
+        .collapsed-main {
+            margin-left: 0 !important;
+        }
     </style>
 </head>
+
 <body>
     <div class="navbar">
-        <div class="navbar-brand"><?php echo str_replace('-', ' ', basename($_SERVER['SCRIPT_FILENAME'], ".php")); ?></div>
-       
+        <button id="burger-icon" class="btn-burger hidden">&#9776;</button>
+        <div class="navbar-brand"><?php echo str_replace('-', ' ', basename($_SERVER['SCRIPT_FILENAME'], ".php")); ?>
+        </div>
+
     </div>
 </body>
+
 </html>
