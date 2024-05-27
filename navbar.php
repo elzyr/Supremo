d9f2edb4035e2bfa0be124432069dc491aea2eb3
KHUJOLT <ilya.khotim@gmail.com>
2024-05-27 10:22:32 +0200

    deleted title

--- navbar.php
+++ navbar.php
@@ -5,7 +5,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nawigacja</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="navbar.css"> <!-- Nowe połączenie do pliku CSS -->
 </head>
