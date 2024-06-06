72a1ddcb4c00f8614af403ad78962814e8f3d57b
KHUJOLT <ilya.khotim@gmail.com>
2024-06-06 11:12:36 +0200

    top-navbar user greetings

--- top-navbar.php
+++ top-navbar.php
@@ -1,3 +1,4 @@
+<?php require ("php/verifyUser.php") ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,11 +9,23 @@
 </head>
 
 <body>
-    <div class="navbar">
-        <button id="burger-icon" class="btn-burger hidden-burger">&#9776;</button>
-        <div class="navbar-brand"><?php echo ucwords(str_replace('-', ' ', basename($_SERVER['SCRIPT_FILENAME'], ".php"))); ?>
+    <div class="navbar"> <!--TODO: Cześć, User!-->
+        <div class="top-nav-left">
+            <button id="burger-icon" class="btn-burger hidden-burger">&#9776;</button>
+            <div class="navbar-brand">
+                <?php echo ucwords(str_replace('-', ' ', basename($_SERVER['SCRIPT_FILENAME'], ".php"))); ?>
+            </div>
         </div>
-
+        <div class="top-nav-right">
+            <span id="greet-user">
+                <?php if ($user->getName()) {
+                    echo "Cześć, " . $user->getName() . "!";
+                } else {
+                    echo "Jesteś niezalogowany! Co tu robisz?";
+                }
+                ?>
+            </span>
+        </div>
     </div>
     <script src="toggle-navbar.js"></script>
 </body>
